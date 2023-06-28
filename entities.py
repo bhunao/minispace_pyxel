285247def5e47828cf8bf7e3a3da33e58bbe4b05
bhunao <bhunao@gmail.com>
2023-06-27 21:55:17 -0300

    add: hud

--- entities.py
+++ entities.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Callable, Optional
+from typing import Optional
 from engine.components import StateRender, Movement, Combat
 
 
@@ -12,30 +12,65 @@ FIREBALL = (
 class Player(StateRender, Movement):
     hp: int = 3
 
-    def skill_1(self, add):
-        add(
+    def skill_1(self, world):
+        world.add(
             Projectile(x=self.x, y=self.y, angle=-
                        90, speed=3, origin=type(self),
-                       sprite=FIREBALL, damage=1)
+                       sprite=FIREBALL, damage=1,
+                       duration=15)
         )
 
-    def skill_2(self, add):
+    def skill_2(self, world):
         projs = [
             Projectile(x=self.x, y=self.y, angle=angle, speed=3, origin=type(self),
-                       sprite=FIREBALL, damage=1) for angle in range(0, 360, 360//8)
+                       sprite=FIREBALL, damage=1, duration=15) for angle in range(0, 360, 360//8)
         ]
-        print(*[proj.angle for proj in projs])
-        add(*projs)
+        world.add(*projs)
+
+    def skill_3(self, world):
+        projs = [
+            Projectile(x=self.x-8, y=self.y-8, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x, y=self.y-8, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x+8, y=self.y-8, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x-8, y=self.y, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x+8, y=self.y, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x-8, y=self.y+8, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x, y=self.y+8, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+            Projectile(x=self.x+8, y=self.y+8, origin=type(self),
+                       sprite=FIREBALL, damage=1, duration=15),
+        ]
+        world.add(*projs)
 
 
 @dataclass
 class Enemy(StateRender, Movement, Combat):
     hp: int = 3
 
+    def update(self, world):
+        self.move()
+        if self.hp <= 0 and world:
+            world.remove(self)
+
 
 @dataclass
 class Projectile(StateRender, Movement, Combat):
     origin: Optional[type] = None
+    duration: Optional[int] = None
+
+    def update(self, world=None):
+        self.move()
+        if self.duration is not None:
+            self.duration -= 1
+        if self.duration and self.duration <= 0:
+            if world:
+                world.remove(self)
 
     def collide_with_target(self, other):
         coisa = self.collide_with(other) and self.origin != type(other)
