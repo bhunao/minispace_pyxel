ce503a024751cc7cde1ceb72c68018754ca07e93
bhunao <bhunao@gmail.com>
2023-06-29 19:04:01 -0300

    add: stuff

--- entities.py
+++ entities.py
@@ -1,11 +1,31 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import Optional
 from engine.components import StateRender, Movement, Combat
 
 
-FIREBALL = (
-    (2, 8, 16, 5, 8, 7),
-)
+class Sides(Enum):
+    LEFT = 180
+    DOWN_LEFT = -1, 1
+    DOWN = -90
+    DOWN_RIGHT = 1, 1
+    RIGHT = 0
+    UP_RIGHT = 1, -1
+    UP = 90
+    UP_LEFT = -1, -1
+
+
+LEFT = Sides.LEFT
+RIGHT = Sides.RIGHT
+UP = Sides.UP
+DOWN = Sides.DOWN
+
+FIREBALL = {
+    LEFT: ((2, 8, 24, 8, 5, 7),),
+    RIGHT: ((2, 8, 24, -8, 5, 7),),
+    UP: ((2, 8, 16, 5, -8, 7),),
+    DOWN: ((2, 8, 16, 5, 8, 7),),
+}
 
 
 @dataclass
@@ -13,10 +33,13 @@ class Player(StateRender, Movement):
     hp: int = 3
 
     def skill_1(self, world):
+        print(self.current_state, *FIREBALL.keys(), sep="\n")
+        print(self.current_state in FIREBALL.keys())
+
         world.add(
-            Projectile(x=self.x, y=self.y, angle=-
-                       90, speed=3, origin=type(self),
-                       sprite=FIREBALL, damage=1,
+            Projectile(x=self.x, y=self.y, angle=self.current_state.value,
+                       speed=3, origin=type(self),
+                       sprite=FIREBALL[self.current_state], damage=1,
                        duration=15)
         )
 
