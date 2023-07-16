daa323bee348a76fd9431b0021e3f38d3d347dfe
bhunao <bhunao@gmail.com>
2023-07-16 19:38:29 -0300

    add: html version

---
+++ waves.py
@@ -0,0 +1,22 @@
+from entities import enemy1, enemy2, enemy3, enemy4
+
+
+waves = [
+    [enemy4, (), 1, False],
+    [enemy1, (), 1, False],
+    [enemy1, (), 3, False],
+    [enemy1, (), 2, True],
+    [enemy2, (), 1, True],
+    [enemy1, (), 10, False],
+    [enemy3, (), 2, False],
+    [enemy1, (), 10, False],
+    [enemy1, (), 5, False],
+    [enemy1, (), 10, False],
+    [enemy1, (), 15, False],
+    [enemy3, (), 5, False],
+    [enemy1, (), 13, False],
+    [enemy1, (), 8, False],
+    [enemy1, (), 13, False],
+    [enemy1, (), 18, False],
+    [enemy4, (), 1, False],
+]
